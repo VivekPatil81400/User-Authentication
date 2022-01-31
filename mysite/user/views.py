c979f120c123c8b86235503eac9b2f81d6405edf
VivekPatil81400 <59966933+VivekPatil81400@users.noreply.github.com>
2022-01-31 15:43:39 +0100

    first commit

---
+++ mysite/user/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import User
+
+
+# Create your views here.
+def home(request):
+    if 'user' in request.session:
+        current_user = request.session['user']
+        param = {'current_user': current_user}
+        return render(request, 'base.html', param)
+    else:
+        return redirect('login')
+    return render(request, 'login.html')
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        uname = request.POST.get('uname')
+        pwd = request.POST.get('pwd')
+        # print(uname, pwd)
+        if User.objects.filter(username=uname).count()>0:
+            return HttpResponse('Username already exists.')
+        else:
+            user = User(username=uname, password=pwd)
+            user.save()
+            return redirect('login')
+    else:
+        return render(request, 'signup.html')
+
+
+
+def login(request):
+    if request.method == 'POST':
+        uname = request.POST.get('uname')
+        pwd = request.POST.get('pwd')
+
+        check_user = User.objects.filter(username=uname, password=pwd)
+        if check_user:
+            request.session['user'] = uname
+            return redirect('home')
+        else:
+            return HttpResponse('Please enter valid Username or Password.')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    try:
+        del request.session['user']
+    except:
+        return redirect('login')
+    return redirect('login')
+
+def session_info(request):
+    if 'user' in request.session:
+        user = request.session['user']
+        request.session.modified = True
+        return render(request, 'session_info.html', {'user':user})
+    else:
+        return HttpResponse("The session has expired")
